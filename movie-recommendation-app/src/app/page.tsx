29bb4bec946f77bd072d74b4fe71507394f9834a
Eyob Abera <eyobaberabekru@gmail.com>
2026-02-03 14:14:28 +0300

    Create page.tsx
    created the home page here

---
+++ movie-recommendation-app/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>🎬 Movie Recommendation App</h1>
+      <p>Welcome! Select a movie to see recommendations.</p>
+    </main>
+  );
+}
