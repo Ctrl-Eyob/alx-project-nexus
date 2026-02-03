1d8cca60e0401454aac57303ba953b8420a48b13
Eyob Abera <eyobaberabekru@gmail.com>
2026-02-03 13:54:43 +0300

    Update page.tsx
    fixed indentation

--- movie-recommendation-app/src/app/[id]/page.tsx
+++ movie-recommendation-app/src/app/[id]/page.tsx
@@ -1,18 +1,13 @@
 'use client';
-
 import { useParams } from 'next/navigation';
 import { useMovieDetails } from '@/hooks/useMovieDetails';
-
 export default function MoviePage() {
   const params = useParams();
   const id = params?.id as string;
-
   const { movie, loading, error } = useMovieDetails(id);
-
   if (loading) return <p>Loading movie...</p>;
   if (error) return <p>{error}</p>;
   if (!movie) return <p>Movie not found</p>;
-
   return (
     <main style={{ padding: '2rem' }}>
       <h1>{movie.title}</h1>
