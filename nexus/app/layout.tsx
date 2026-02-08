ceed22eb5a0473210763b41b8d9e095f1618ce52
Ctrl-Eyob <eyobaberabekru@gmail.com>
2026-02-08 18:24:58 +0300

    created app/provider.tsx file and updated the layout of the page

--- nexus/app/layout.tsx
+++ nexus/app/layout.tsx
@@ -1,29 +1,5 @@
-'use client';
-import StyledComponentsRegistry from '@/styles/StyledComponentsRegistry';
-import { ThemeProvider } from 'styled-components';
-import { theme } from '@/styles/theme';
+import Providers from './providers';
 import Header from '@/components/Header';
-
-
-export default function RootLayout({
-children,
-}: {
-children: React.ReactNode;
-}) {
-return (
-<html lang="en">
-<body>
-<StyledComponentsRegistry>
-<ThemeProvider theme={theme}>
-<Header />
-{children}
-</ThemeProvider>
-</StyledComponentsRegistry>
-</body>
-</html>
-);
-}
-
 export default function RootLayout({
   children,
 }: {
@@ -32,9 +8,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <StyledComponentsRegistry>
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
-        </StyledComponentsRegistry>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
---
+++ nexus/app/providers.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@/styles/theme';
+import StyledComponentsRegistry from '@/styles/StyledComponentsRegistry';
+
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <StyledComponentsRegistry>
+      <ThemeProvider theme={theme}>
+        {children}
+      </ThemeProvider>
+    </StyledComponentsRegistry>
+  );
+}
