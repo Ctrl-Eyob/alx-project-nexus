e846a464b37fb687944dd31d44f0b409215a46af
Ctrl-Eyob <eyobaberabekru@gmail.com>
2026-02-08 18:14:34 +0300

    updated the layout to display the header section

--- nexus/app/layout.tsx
+++ nexus/app/layout.tsx
@@ -2,6 +2,27 @@
 import StyledComponentsRegistry from '@/styles/StyledComponentsRegistry';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '@/styles/theme';
+import Header from '@/components/Header';
+
+
+export default function RootLayout({
+children,
+}: {
+children: React.ReactNode;
+}) {
+return (
+<html lang="en">
+<body>
+<StyledComponentsRegistry>
+<ThemeProvider theme={theme}>
+<Header />
+{children}
+</ThemeProvider>
+</StyledComponentsRegistry>
+</body>
+</html>
+);
+}
 
 export default function RootLayout({
   children,
